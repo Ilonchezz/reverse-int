82aeafcfe097a7bc9c2f82f07dc12440c3cadd25
Ilona <zaitsavailona@gmail.com>
2024-09-17 00:24:02 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let result = 0;
+  n = Math.abs(n);
+
+  while (n > 0) {
+    const ones = n % 10;
+    result = result * 10 + ones;
+    n = Math.floor(n / 10);
+  }
+
+  return result;
 }
